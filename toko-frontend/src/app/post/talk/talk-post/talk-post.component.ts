386f428905a379cb3c7b0add7d02d8b9c3f40fd0
schwin <schwin.hong@gmail.com>
2022-03-10 00:09:50 -0500

    migrated talk post to dynamic data source

--- toko-frontend/src/app/post/talk/talk-post/talk-post.component.ts
+++ toko-frontend/src/app/post/talk/talk-post/talk-post.component.ts
@@ -12,4 +12,13 @@ export class TalkPostComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  postSrc = {
+    "imgSrc" : "https://d1e00ek4ebabms.cloudfront.net/production/cd51b463-6cb6-4248-a365-1b91ace4edb3.jpg",
+    "des" : "Oven is a typical techbro in silicon valley.",
+    "authorImgSrc" : "assets/img/faces/team-3.jpg",
+    "authorName" : "Joanna Doe",
+    "like" : 25,
+    "comment" : 10
+  }
+
 }
