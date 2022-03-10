bed168b613f4de11dacb5026f367d098831fda0e
schwin <schwin.hong@gmail.com>
2022-03-10 00:13:39 -0500

    migrated ann post to dynamic src

--- toko-frontend/src/app/post/ann/ann-post/ann-post.component.ts
+++ toko-frontend/src/app/post/ann/ann-post/ann-post.component.ts
@@ -12,4 +12,12 @@ export class AnnPostComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  postSrc = {
+    "imgSrc" : "https://images.macrumors.com/t/yKXzq1hk6lH0tceh4OpS-AetjMc=/1600x1200/smart/article-new/2020/03/applestorepaloalto.jpg",
+    "des" : "Apple lifted mask mandate in its stores",
+    "authorImgSrc" : "assets/img/faces/team-3.jpg",
+    "authorName" : "Joanna Doe",
+  }
+
+
 }
